3c1a7d5d5247d4400941d631225804226e4de9f0
ANMOL <sonianmol0918@gmail.com>
2025-07-18 20:35:53 +0530

    PREDICTION_GAME

---
+++ PREDICTION_GAME.py
@@ -0,0 +1,36 @@
+#snake water gun
+print("SNAKE WATER GUN GAME \nMAKE FIRST 3 POINTS TO WIN FROM COMPUTER")
+import random
+c=['snake','water','gun']
+name=str(input("ENTER YOUR NAME:-"))
+
+score=0
+computer=0
+n=0
+while score<3 and computer<3:
+  n+=1
+  user=str(input(f"\nChoose:-SNAKE or WATER or GUN\n\n{n}:-{name}:-"))
+  if user=='snake' or user=='water' or user=='gun':
+        random_c = random.choice(c)
+        x=random_c
+        print("COMPUTER:-",x)
+        if user==x:
+            score+=1
+            print(f"good +1\n\t\t\tcomputer={computer}\n\t\t\t{name}={score}")
+            continue
+        else :
+                computer+=1
+                print(f"hahaha\t\t\t\n\t\t\tcomputer={computer}\n\t\t\t{name}={score}")
+                continue
+  else:
+                    print(f"wrong\nCheck your input {name}")
+                    print(f"\t\t\t{name}={score}\n")
+                    continue
+else :
+                        print(f"\t\t\tGAME COMPLETED")
+                        if computer==3:
+                            print(f"\nCOMPUTER WINS")
+                        elif score==3:
+                                print(f"{name} WINS")
+                        else :
+                                    print("DRAW")
